44e7a6ced2beb73d5ac5fdd4e6e203cb7af7256e
Anneparvin <108586988+Anneparvin@users.noreply.github.com>
2023-06-08 22:53:11 +0600

    add6

--- src/Components/Pages/Home/Gallery/Gallery.jsx
+++ src/Components/Pages/Home/Gallery/Gallery.jsx
@@ -7,6 +7,8 @@ import gallery5 from '../../../../assets/images/Gallery/male-3951486_640.jpg';
 import gallery6 from '../../../../assets/images/Gallery/music-5724427_640.jpg';
 import gallery7 from '../../../../assets/images/Gallery/puzzle-ball-1729042_640.jpg';
 import gallery8 from '../../../../assets/images/Gallery/toy-3567266_640.jpg';
+import gallery9 from '../../../../assets/images/Gallery/toys-993166_640.jpg';
+import gallery10 from '../../../../assets/images/Gallery/music-box-2632297_640.jpg';
 
 const Gallery = () => {
     return (
@@ -15,7 +17,7 @@ const Gallery = () => {
             <h1 className='text-6xl font-bold mt-8'>Gallery Side</h1>
             <h3 className='font-bold'>With Little People figures, toddlers can explore how exciting is the world around them!</h3>
         <p className='text-xl bg-red-700 p-8 w-full rounded-xl font-bold mt-8'>Get FREE delivery on orders over R800 or Click & Collect nationwide at Toys R Us</p>
-            <div className='grid grid-cols-2 md:grid-cols-4 gap-2 md:gap-4 bg-black p-8 rounded-xl mt-2'>
+            {/* <div className='grid grid-cols-2 md:grid-cols-4 gap-2 md:gap-4 bg-black p-8 rounded-xl mt-2'>
                 <img className='w-full' src={gallery1} alt='pic'/>
                 <img className='w-full' src={gallery2} alt='pic'/>
                 <img className='w-full' src={gallery3} alt='pic'/>
@@ -24,7 +26,22 @@ const Gallery = () => {
                 <img className='w-full' src={gallery6} alt='pic'/>
                 <img className='w-full' src={gallery7} alt='pic'/>
                 <img className='w-full' src={gallery8} alt='pic'/>
-            </div>
+            </div> */}
+
+<section className="py-6 dark:bg-gray-800 dark:text-gray-50">
+	<div className="container grid grid-cols-2 gap-4 p-4 mx-auto md:grid-cols-4 bg-black rounded-xl mt-2">
+		<img src={gallery1} alt="" className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-3 md:row-start-1 dark:bg-gray-500 aspect-square" />
+		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src={gallery2} />
+		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src={gallery3} />
+		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src={gallery4} />
+		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src={gallery5} />
+		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src={gallery6} />
+		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src={gallery7}/>
+		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src={gallery8}/>
+		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src={gallery9} />
+		<img src={gallery10} alt="" className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-1 md:row-start-3 dark:bg-gray-500 aspect-square" />
+	</div>
+</section>
             
         </div>
     );
